310304b26dc5d367cf840ccd43914c103dfe884e
Marcin Arkan <marcinxe@gmail.com>
2020-08-23 14:59:47 +0200

    Update conversion script to new inkscape cli syntax

--- PNGs/ConvertSvgsToPngs.sh
+++ PNGs/ConvertSvgsToPngs.sh
@@ -11,7 +11,7 @@ cp ../SVGs/*.svg ./
 for svgFile in *.svg; do
      pngFile="${svgFile%.svg}.png"
 
-     inkscape -z -e "Square/${pngFile}" -w 1108 -h 1108 "${svgFile}"
+     inkscape --export-filename="Square/${pngFile}" -w 1108 -h 1108 "${svgFile}"
      gm composite -compose CopyOpacity ../Masks/CircleMask.png "Square/${pngFile}" "${shape}/${pngFile}"
      gm composite -compose CopyOpacity ../Masks/TeardropMask.png "Square/${pngFile}" "Teardrop/${pngFile}"
      gm composite -compose CopyOpacity ../Masks/RoundedSquare30pxMask.png "Square/${pngFile}" "RoundedSquare30px/${pngFile}"
